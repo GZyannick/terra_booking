a73dc2d135fac9c79fc23118e51fd745ab1066b4
Yannick Gouez <ygouez5757@gmail.com>
2020-06-08 17:42:17 +0200

    seed fini

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,5 +15,8 @@ superman = User.create(username: "Superman", email: "clark@terre.tr", password: 
 
 
 saturne = Planet.create(owner: alpha, name: "saturne", description: "planete non tellurique , belle vue sur la terre")
-mars = Planet.create(name: "mars", description: "belle vue sur saturne")
-vulcain = Planet.create(name: "vulcain", description: "pas de belle vue , venez comme vous etes")
+mars = Planet.create(owner: spock, name: "mars", description: "belle vue sur saturne")
+vulcain = Planet.create(owner: spock, name: "vulcain", description: "pas de belle vue , venez comme vous etes")
+
+booking1 = Booking.create!(duration: 1, user: blob, planet: vulcain)
+booking2 = Booking.create!(duration: 7, user: spock, planet: saturne)
