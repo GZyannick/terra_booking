4a4cb38338f16541cbcc6a9a296bd052ae50c43e
Yannick Gouez <ygouez5757@gmail.com>
2020-06-08 17:00:39 +0200

    seed init

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
-alpha = User.create(username: "AlPha", email: "planet@saturne.st", password: "azerty" , first_name: 'Al', last_name: 'Pha', owned_planets: "saturne")
+Planet.destroy_all
+User.destroy_all
+
+
+
+alpha = User.create(username: "AlPha", email: "planet@saturne.st", password: "azerty" , first_name: 'Al', last_name: 'Pha')
+
 
 blob = User.create(username: "BlobloBg", email: "planet@mars.ms", password: "azerty" , first_name: 'blo', last_name: 'B')
+
+spock = User.create(username: "Spock", email: "spock@vulcain.vm", password: "azerty" , first_name: 'spo', last_name: 'ck')
+
+
+superman = User.create(username: "Superman", email: "clark@terre.tr", password: "azerty" , first_name: 'Clark', last_name: 'kent')
+
+
+saturne = Planet.create(owner: alpha, name: "saturne", description: "planete non tellurique , belle vue sur la terre")
+mars = Planet.create(name: "mars", description: "belle vue sur saturne")
+vulcain = Planet.create(name: "vulcain", description: "pas de belle vue , venez comme vous etes")
