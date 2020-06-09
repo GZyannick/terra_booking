554f1444ac4e79fe4470130d8b1608f1c60fbbe8
Yannick Gouez <ygouez5757@gmail.com>
2020-06-09 14:16:14 +0200

    ajout des controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,28 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
   end
 
   def create
+    @booking = Booking.new(booking_params)
+    @booking.user = current_user
+    #@planet.owner = User.find(1)
+    if @booking.save!
+      redirect_to user_path(@booking.user), notice: "Congrats! You have successfully booked this planet!"
+    else
+      render :new
+    end
   end
 
-  def destroy
+#  def destroy
+#    if @booking.destroy!
+#      redirect_to user_path(@booking.user), notice: 'This #booking was successfully deleted!'
+#    else
+#      redirect_to user_path(@booking.user), notice: "Sorry, #we need to reload our destroying machine..."
+#    end
+#  end
+  private
+  def booking_params
+    params.require(:booking).permit(:duration, :planet_id)
   end
 end
--- app/controllers/planets_controller.rb
+++ app/controllers/planets_controller.rb
@@ -1,22 +1,54 @@
 class PlanetsController < ApplicationController
+  before_action :find_planet , only: [:show, :edit, :create, :destroy]
+  skip_before_action :authenticate_user!, only: [:home, :show]
+  def home
+  end
   def index
+    @planet = Planet.all
   end
 
   def show
   end
 
   def new
+    @planet = Planet.new
   end
-
+#TODO verifie create is working 
   def create
+    @planet = Planet.new(planet_params)
+    @planet.owner = current_user
+    #@planet.owner = User.find(1)
+    if @planet.save!
+      redirect_to planet_path(@planet)
+    else
+      render :new
+    end
   end
-
+#TODO verifie create is working 
   def edit
   end
 
   def update
+    if planet.update!(planet_params)
+      redirect_to planet_path(@planet), notice: 'This planet was successfully updated!'
+    else
+      render :edit
+    end
   end
 
-    def destroy
+  def destroy
+    if @planet.destroy!
+      redirect_to planets_path, notice: 'This planet was successfully deleted!'
+    else
+      redirect_to planet_path(@planet), notice: "Sorry, we need to reload our destroying machine..."
+    end
+  end
+
+  private
+  def find_planet
+    @planet = Planet.find(:id)
+  end
+  def planet_params
+    params.require(:planet).permit(:name , :description, :price)
   end
 end
